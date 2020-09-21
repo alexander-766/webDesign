722516a2a0225eac9b3b8565068d92ee4e7d057b
alexander-766 <70936048+alexander-766@users.noreply.github.com>
2020-09-21 10:54:26 +0200

    index.php
    Das hier ist die Hauptseite.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="de">
+    <link rel="icon" type="image/png" href="icon.png">
+
+<head>
+    <meta charset="UTF-8">
+    <title> Team Mosella Gaming- alexander.bplaced.net </title>
+    <meta name="Team Mosella" content="Startseite">
+    <link href="design.css" rel="stylesheet">
+</head>
+
+<body>
+   
+    <?php 
+    
+        echo "<h1> Hello World </h1>";
+    
+    
+    ?>
+   
+    
+
+</body>
+
+
+
+</html>
